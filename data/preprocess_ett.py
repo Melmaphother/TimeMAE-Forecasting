9ae93c239379d19bedaf4a7dfdb7d0f9a656a8c9
Melmaphother <1559792558@qq.com>
2024-04-11 23:19:09 +0800

    add pre_process

--- data/preprocess_ett.py
+++ data/preprocess_ett.py
@@ -18,6 +18,24 @@ def window_slicing(data, slicing_size=128):
     )
     return reshaped_data
 
+def window_slicing_forecasting(data, slicing_size=128):
+    n_samples, n_features = data.shape
+    # 计算填充后的总长度
+    padding = (-n_samples) % slicing_size
+    padded_array = np.pad(data, ((0, padding), (0, 0)), 'constant', constant_values=0)
+    # 更新样本数量包括填充的部分
+    n_samples_padded = n_samples + padding
+    n_examples = n_samples_padded // slicing_size
+    # 计算新的strides
+    new_strides = (slicing_size * data.strides[0],) + data.strides
+    # 使用as_strided创建分片视图
+    sliced_array = np.lib.stride_tricks.as_strided(
+        padded_array, 
+        shape=(n_examples, slicing_size, n_features), 
+        strides=new_strides
+    )
+    return sliced_array
+
 
 def preprocess_ett(file_name):
     data = pd.read_csv(f'ETT-small/{file_name}.csv', index_col='date', parse_dates=True)
@@ -41,7 +59,21 @@ def preprocess_ett(file_name):
     valid_data = window_slicing(data[valid_slice])
     test_data = window_slicing(data[test_slice])
 
+    train_data_forecasting = window_slicing_forecasting(data[train_slice])
+    valid_data_forecasting = window_slicing_forecasting(data[valid_slice])
+    test_data_forecasting = window_slicing_forecasting(data[test_slice])
+    print(data[train_slice].shape, data[valid_slice].shape, data[test_slice].shape)
+    print(train_data.shape, valid_data.shape, test_data.shape)
+    print(train_data_forecasting.shape, valid_data_forecasting.shape, test_data_forecasting.shape)
+    """
+    etth1
+    (8640, 7) (2880, 7) (2880, 7)
+    (8513, 128, 7) (2753, 128, 7) (2753, 128, 7)
+    (68, 128, 7) (23, 128, 7) (23, 128, 7)
+    """
+
     train_data, valid_data, test_data = torch.from_numpy(train_data), torch.from_numpy(valid_data), torch.from_numpy(test_data)
+    train_data_forecasting, valid_data_forecasting, test_data_forecasting = torch.from_numpy(train_data_forecasting), torch.from_numpy(valid_data_forecasting), torch.from_numpy(test_data_forecasting)
 
     if not os.path.exists(f'ETT-small/{file_name}'):
         os.makedirs(f'ETT-small/{file_name}')
@@ -50,8 +82,12 @@ def preprocess_ett(file_name):
     torch.save(valid_data, f'ETT-small/{file_name}/val.pt')
     torch.save(test_data, f'ETT-small/{file_name}/test.pt')
 
+    #torch.save(train_data_forecasting, f'ETT-small/{file_name}/train_forecasting.pt')
+    #torch.save(valid_data_forecasting, f'ETT-small/{file_name}/val_forecasting.pt')
+    torch.save(test_data_forecasting, f'ETT-small/{file_name}/test_forecasting.pt')
+
 if __name__ == '__main__':
     preprocess_ett('ETTh1')
-    preprocess_ett('ETTh2')
-    preprocess_ett('ETTm1')
-    preprocess_ett('ETTm2')
+    #preprocess_ett('ETTh2')
+    #preprocess_ett('ETTm1')
+    #preprocess_ett('ETTm2')
